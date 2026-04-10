f50415ae4d2b6f1f1a3c5bbe8c12e028035ac021
sachinsinha0 <sachin.sinha@greatlearning.in>
2026-04-10 12:57:49 +0530

    Add Design Mirror cask formula

---
+++ Casks/design-mirror.rb
@@ -0,0 +1,19 @@
+cask "design-mirror" do
+  version "1.0"
+  sha256 "70089a4462803c9c9e4b69a94a0b4658ab508d7de4952151fe50ae0973de9692"
+
+  url "https://github.com/sachinsinha0/design-mirror/releases/download/v#{version}/Design.Mirror.dmg"
+  name "Design Mirror"
+  desc "Mirror your web pages into Figma"
+  homepage "https://github.com/sachinsinha0/design-mirror"
+
+  app "Design Mirror.app"
+
+  uninstall quit: "com.designmirror.app"
+
+  zap trash: [
+    "~/Library/Application Support/Design Mirror",
+    "~/Library/Preferences/com.designmirror.app.plist",
+    "/tmp/figma-pusher-runtime",
+  ]
+end
