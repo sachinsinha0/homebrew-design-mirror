670a366153fb44ec78e7b31addc4dda3a975f50a
sachinsinha0 <sachin.sinha@greatlearning.in>
2026-04-12 14:54:53 +0530

    Point downloads to design-mirror-releases repo

--- Casks/design-mirror.rb
+++ Casks/design-mirror.rb
@@ -2,10 +2,10 @@ cask "design-mirror" do
   version "1.3.2"
   sha256 "78f3e138f86ebe3991f6c8ec679f338966e08b8e50cf1a8893e6fa721b720478"
 
-  url "https://github.com/sachinsinha0/design-mirror/releases/download/v#{version}/Design.Mirror.dmg"
+  url "https://github.com/sachinsinha0/design-mirror-releases/releases/download/v#{version}/Design.Mirror.dmg"
   name "Design Mirror"
   desc "Mirror your web pages into Figma"
-  homepage "https://github.com/sachinsinha0/design-mirror"
+  homepage "https://github.com/sachinsinha0/design-mirror-releases"
 
   app "Design Mirror.app"
 
